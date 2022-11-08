014243b9a5221efbabd32bce2e0a28b555500c31
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2022-11-08 12:10:09 +0100

    Downloading python

--- src/legcord.nim
+++ src/legcord.nim
@@ -1,7 +1,18 @@
 import tinydialogs
 import strformat
 import os
+import puppy
 import osproc
+type
+  pythonDownload = object
+    platform: string
+    url: string
+    fileExtension: string
+
+let download = [
+    pythonDownload(platform: "linux", url:"https://github.com/indygreg/python-build-standalone/releases/download/20221106/cpython-3.8.15+20221106-x86_64-unknown-linux-gnu-pgo+lto-full.tar.zst", fileExtension: ".tar.zst"),
+    pythonDownload(platform: "windows", url:"placeholder", fileExtension: ".zip")
+]
 
 proc install(pythonPath: string) =
     echo fmt"Using `{pythonPath}` executable"
@@ -24,4 +35,8 @@ else:
             if choice == Cancel:
                 echo messageBox("Legcord Installer", "Installation has failed. Please either install a working Python3 install or let Legcord install it for you.", Ok, Error, Yes)
             elif choice == Yes:
-                echo "yes"
+                echo "Installing python"
+                for pythonDownload in download:
+                    if pythonDownload.platform == hostOS:
+                        writeFile("python" & pythonDownload.fileExtension, fetch(pythonDownload.url))
+                        
